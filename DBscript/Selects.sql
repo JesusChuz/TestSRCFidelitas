079ef103089377d330983fd12ffaa89cf94d88b1
JesusChuz <j.mendezquesada18@gmail.com>
2023-06-22 18:22:27 -0600

    Sprin 2 98% completado

---
+++ DBscript/Selects.sql
@@ -0,0 +1,33 @@
+use SIST_RECONOCIMIENTO;
+use SIST_RECONOCIMIENTO;
+
+SELECT * FROM AspNetRoleClaims;
+SELECT * FROM AspNetRoles;
+SELECT * FROM AspNetUserClaims;
+SELECT * FROM AspNetUserLogins;
+SELECT * FROM AspNetUserRoles;
+SELECT * FROM Engineers;
+SELECT * FROM AspNetUsers;
+SELECT * FROM __EFMigrationsHistory;
+
+Delete from AspNetUsers
+Delete from AspNetUserRoles
+
+SELECT * FROM CSAT;
+SELECT * FROM Phrases;
+SELECT * FROM Engineers;
+SELECT * FROM Log_PasswordUpdate;
+SELECT * FROM Positions;
+SELECT * FROM Rewards;
+SELECT * FROM Purchases;
+SELECT * FROM Manager;
+
+SELECT * FROM Recognitions;
+SELECT * FROM Rol;
+
+
+DELETE FROM Recognitions
+DELETE FROM Positions
+DELETE FROM Engineers
+
+DELETE FROM AspNetUsers where Email = 'john@gmail.com';
